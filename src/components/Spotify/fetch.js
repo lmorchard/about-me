027751b6f9e9c89d42ecb467b02112e1761b3312
Les Orchard <me@lmorchard.com>
2017-10-04 02:43:58 -0400

    Start on a Spotify recently played component

---
+++ src/components/Spotify/fetch.js
@@ -0,0 +1,32 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+export default function fetchData(config) {
+  const {
+    username,
+    clientId,
+    clientSecret,
+    access_token,
+    refresh_token
+  } = config;
+
+  const spotifyApi = new SpotifyWebApi({ clientId, clientSecret });
+  spotifyApi.setRefreshToken(refresh_token);
+  spotifyApi.setAccessToken(access_token);
+
+  let user, tracks;
+
+  return spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      spotifyApi.setAccessToken(data.body['access_token']);
+      return spotifyApi.getMe();
+    })
+    .then(data => {
+      user = data.body;
+      return spotifyApi.getMyRecentlyPlayedTracks();
+    })
+    .then(data => {
+      tracks = data.body.items;
+      return { username, user, tracks };
+    });
+}
