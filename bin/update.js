2d9da63aeb2ff1ee24fe04949fb81363035a63fd
Les Orchard <me@lmorchard.com>
2018-08-22 01:34:28 -0400

    Tweak config layout a bit

--- bin/update.js
+++ bin/update.js
@@ -3,26 +3,25 @@ require("babel-register")({ presets: ["node8", "react"] });
 
 const fs = require("fs");
 const { all, map } = require("../src/lib/utils");
-const config = require("../config");
-const toFetch = Object.keys(config);
+const toFetch = require("../config").fetch;
 
-const fetchPath = (config, name) => {
-  const path = config.component ? config.component : name;
+const fetchPath = (fetchConfig, name) => {
+  const path = fetchConfig.component ? fetchConfig.component : name;
   return `../src/components/${path}/fetch.js`;
 };
 
-const fetchDataSource = async name => {
+const fetchDataSource = async ([name, fetchConfig]) => {
   const startTime = Date.now();
   console.log(`${startTime} start ${name}`);
-  const fetcher = require(fetchPath(config[name], name)).default;
-  const result = await fetcher(config[name], name);
+  const fetcher = require(fetchPath(fetchConfig, name)).default;
+  const result = await fetcher(fetchConfig, name);
   const endTime = Date.now();
   console.log(`${endTime} finish ${name} (${endTime - startTime}ms)`);
   return result;
 };
 
 async function main() {
-  const results = await map(toFetch, fetchDataSource);
+  const results = await map(Object.entries(toFetch), fetchDataSource);
   const state = results.reduce(
     (acc, data, idx) => ({ ...acc, [toFetch[idx]]: data }),
     {}
