81071d503c80df0cdf6e024ad20ffff10a336562
Les Orchard <me@lmorchard.com>
2020-06-12 16:50:00 -0700

    Resolve relative URLs in the note card

--- cards/Note/fetch.js
+++ cards/Note/fetch.js
@@ -1,15 +1,29 @@
+const url = require("url");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
 module.exports = async function fetchData(config) {
-  const { url, title, contentSelector = "section.post-content" } = config;
-  const resp = await fetch(url);
+  const {
+    url: noteUrl,
+    title,
+    baseUrl = "https://lmorchard.github.io/notes/",
+    contentSelector = "section.post-content",
+  } = config;
+  const resp = await fetch(noteUrl);
   const data = await resp.text();
   const $ = cheerio.load(data);
 
+  // HACK: make all the relative links absolute with respect to the notes base URL
+  $("a").each((idx, _el) => {
+    const el = $(_el);
+    const href = el.attr("href");
+    const absHref = url.resolve(baseUrl, href);
+    el.attr("href", absHref);
+  });
+
   return {
     title,
-    url,
+    url: noteUrl,
     content: $(contentSelector).html(),
   };
 };
